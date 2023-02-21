e1dbbbd84bf9dbe31873f352ad8ccd42d860e63c
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-21 13:04:44 -0600

    Services, httpClient y data externa

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { DataService } from './../../services/data.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.sass']
+  styleUrls: ['./posts.component.sass'],
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+  constructor(private dataService: DataService) {}
 
+  ngOnInit(): void {
+    this.dataService.posts.subscribe((posts) => {
+      console.log(posts);
+    });
+  }
 }
