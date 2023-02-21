4df251721ef3df310ed85b3acf250e9a8c9c69ed
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-21 12:27:41 -0600

    routerLink & menuComponent

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.sass'],
+})
+export class MenuComponent {
+  routes = [
+    {
+      name: 'Home',
+      path: '/home',
+    },
+    {
+      name: 'About',
+      path: '/about',
+    },
+    {
+      name: 'Contact',
+      path: '/contact',
+    },
+  ];
+}
