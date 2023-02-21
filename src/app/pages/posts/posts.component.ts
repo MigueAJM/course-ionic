c959990e9882c5548b13b51b857eedabebf7345a
Miguel Angel Jimenez <miguejm@outlook.es>
2023-02-21 15:31:09 -0600

    observables y subscripciones|async operador tap: rxjs/operator

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { DataService } from './../../services/data.service';
 import { Component, OnInit } from '@angular/core';
+import { Posts } from '../../interfaces/posts';
 
 @Component({
   selector: 'app-posts',
@@ -7,11 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.sass'],
 })
 export class PostsComponent implements OnInit {
+  /* messages: Posts[] = []; */
+  messages: any;
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.dataService.posts.subscribe((posts) => {
-      console.log(posts);
-    });
+    /**
+     * * |async:
+     * ?  para cancelar la subcripcion de forma automatica y no indicarla en el "ngOnDestroy"
+     * this.dataService.posts.subscribe((posts: any) => {
+     * this.messages = posts;
+    }); */
+    this.messages = this.dataService.posts;
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+/**
+ * ? operator "tap":
+ * * ejecutar una acción cuando se recibe una subscripción, pero no tiene efectos secundarios
+ * * transforma la "data".
+ */
+import { tap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -8,6 +15,9 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   get posts() {
-    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+    return this.http
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .pipe(tap(console.log));
+    //.pipe(tap((posts) => console.log(posts)));
   }
 }
